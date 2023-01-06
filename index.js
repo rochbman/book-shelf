b6d4d6c936e1a1f9683a79322098baad66d7034f
rochbman <real@email.co>
2023-01-06 17:17:55 -0500

    fav list and started sort by title or author and search bar

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ let main =document.querySelector(".main")
 let ul;
 let bookinfo = document.querySelector(".bookInfo");
 let b;
-let button = document.querySelector('button')
+let button = document.querySelector('#add');
+let favButt = document.querySelector('#fav');
+let favList = document.querySelector('#favList');
+let allBooks = document.querySelector('#allBooks');
+let titleSort = document.querySelector('#title');
+let authorSort = document.querySelector('#author');
+let favArr =[];
+
+const searchInput = document.querySelector('.input');
+const clearButton = document.querySelector('#clear');
+const list = document.querySelector('#list')
 
 //bookshelf class set up
 
@@ -16,26 +26,162 @@ class Bookshelf{
 
     //loops over book array and renders a book for each object in the array
 
-    render(){
-        for(let i=0; i<this.bookArr.length;i++){
-            b = new Book(this.bookArr[i].author, this.bookArr[i].language, this.bookArr[i].subject, this.bookArr[i].title);
-           ul= main.appendChild(document.createElement('ul'));
+    render(arr){
+        let bookcount = 0;
+        const shelf = arr.map((obj)=>{
+                 b = new Book(obj.author, obj.language, obj.subject, obj.title);
+           ul= main.appendChild(document.createElement('li'));
+           
            // add a class name of i to equal the index of that book in the book array so i can access the book obj with its class name
-            ul.className=`${i}`;
+
+            ul.id=`${bookcount}`;
            // diplays book title and author for each book
-            ul.innerHTML=this.bookArr[i].title+" by: " +this.bookArr[i].author[0] 
-        }
+            ul.innerHTML=obj.title+" by: " +obj.author[0];
+            bookcount++ 
+        })
     }
 
     //adds event listener to each ul created above on click each books information will be displayed 
 
     clickclack(){
+       
         for(let i=0; i<this.bookArr.length;i++){
-            let u=document.getElementsByClassName(`${i}`);
-            u[0].addEventListener('click',function(){
-                bookinfo.innerHTML=`Your book is ${bookData[i].title} by ${bookData[i].author} it is writen in ${bookData[i].language}. This book's subjects include' ${bookData[i].subject} `
+            let u=document.getElementById(`${i}`);
+            u.addEventListener('click',function(){
+                bookinfo.innerHTML=`Your book is ${bookData[i].title} by ${bookData[i].author} it is writen in ${bookData[i].language}. This book's subjects include' ${bookData[i].subject} `;
+                //make favorite button apper when a book is selected
+                
+                favButt.style.display='block';
+
+                // give button listener to push that books obj into an array of favorite books
+                favButt.addEventListener('click',function(){
+                    favArr.push(bookData[i]);
+                    //sort and filter to make sure the are no dups
+                    favArr.sort()
+                    let filtered= favArr.filter(function(item, pos) {
+                        return favArr.indexOf(item) == pos;
+                    })
+                    favArr=filtered
+                    favButt.style.display="none"
+                    
+
+                })
             })
         } 
+
+        //add listener to fav books button to clear bookshelf and diplay only users favorited books
+        favList.addEventListener('click',function(){
+            
+            document.querySelectorAll("li").forEach(el => el.remove());
+            let favShelf= new Bookshelf(favArr)
+            // console.log(favArr)
+            favShelf.render(favArr)
+            favShelf.clickclack(favArr)
+            
+
+        })
+
+        //add listener to all books button to display all books again and it will still include any books the user added
+
+        allBooks.addEventListener('click',function(){
+
+            document.querySelectorAll("li").forEach(el => el.remove());
+            const yourBS= new Bookshelf(bookData);
+            // console.log(bookData);
+            yourBS.render(bookData);
+            yourBS.clickclack(bookData);
+            
+
+        })
+
+        // trying to sort by title and author need to find out how to reference both properly here to make these work
+
+        titleSort.addEventListener('click', function(){
+            document.querySelectorAll("li").forEach(el => el.remove());
+            this.bookArr.sort(function(a, b) {
+                if (a < b) {
+                  
+                  return -1;
+                }
+                if (a > b) {
+                  
+                  return 1
+                }
+                return 0
+                
+              })
+              console.log(bookArr);
+            //   const sortedBs= new Bookshelf(bookArr);
+              
+            //   sortedBs.render(bookArr);
+            //   sortedBs.clickclack(bookArr);
+              
+
+        })
+
+        authorSort.addEventListener('click', function(){
+            document.querySelectorAll("li").forEach(el => el.remove());
+            this.bookArr.sort(function(a, b) {
+                if (a < b) {
+                  
+                  return -1;
+                }
+                if (a > b) {
+                  
+                  return 1
+                }
+                return 0
+                
+              })
+              console.log(this.bookArr);
+            //   const sortedBs= new Bookshelf(bookData);
+              
+            //   sortedBs.render(bookData);
+            //   sortedBs.clickclack(bookData);
+              
+
+        })
+
+        searchInput.addEventListener("input", (e) => {
+            let value = e.target.value
+        
+            if (value && value.trim().length > 0){
+                 value = value.trim().toLowerCase()
+        
+                //returning only the results of setList if the value of the search is included in the person's name
+                setList(bookData.filter(foundBook => {
+                    return foundBook.title.includes(value)
+                }))}})
+        function setList(results){
+
+            for (const foundBook of results){
+                // creating a li element for each result item
+                const resultItem = document.createElement('li')
+        
+                // adding a class to each item of the results
+                resultItem.classList.add('result-item')
+        
+                // grabbing the name of the current point of the loop and adding the name as the list item's text
+                const text = document.createTextNode(foundBook.name)
+        
+                // appending the text to the result item
+                resultItem.appendChild(text)
+        
+                // appending the result item to the list
+                list.appendChild(resultItem)
+            }
+        }
+
+        function clearList(){
+            // looping through each child of the search results list and remove each child
+            while (list.firstChild){
+                list.removeChild(list.firstChild)
+            }
+        }
+
+        clearButton.addEventListener("click", () => {
+            clearList()
+        })
     }
 }
 
@@ -53,7 +199,7 @@ class Book{
 //create new bookshelf the render all books and add the event listeners
 
 let bs= new Bookshelf(bookData)
-bs.render()
+bs.render(bookData)
 bs.clickclack()
 
 
@@ -81,13 +227,13 @@ button.addEventListener('click',function(){
     
     //delete all books
 
-    document.querySelectorAll("ul").forEach(el => el.remove());
+    document.querySelectorAll("li").forEach(el => el.remove());
     
     //  adds all books again but new list has the users book included
 
     bookData.push(yourBook);
     const yourBS= new Bookshelf(bookData);
     console.log(bookData);
-    yourBS.render();
-    yourBS.clickclack();
+    yourBS.render(bookData);
+    yourBS.clickclack(bookData);
 });
